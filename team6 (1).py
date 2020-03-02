0540ba9bf81a0a4606b82c17c6920df82c336376
Chettina <61426939+Chettina@users.noreply.github.com>
2020-03-02 09:17:33 -0800

    Update team6 (1).py

--- team6 (1).py
+++ team6 (1).py
@@ -8,7 +8,7 @@
 
 team_name = 'Gabanne' # Only 10 chars displayed.
 strategy_name = 'da nuggies'
-strategy_description = 'How does this strategy decide?'
+strategy_description = 'very goodly'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
