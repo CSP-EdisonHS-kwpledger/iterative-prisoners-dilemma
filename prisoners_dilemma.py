6fb02fb74c24e2d41cf540f6989f1e7c4625a54c
Kevin <kwpledger@users.noreply.github.com>
2020-03-02 09:31:21 -0800

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -31,21 +31,17 @@ from __future__ import print_function
 # section0, section1, section2, section3 = reports
 #######
 import random
-import os.path              
+import os.path
+import imp              
     
 import example0, example1, example2, example3
 import example4, example5, example6, example7
-import team0, team1, team2, team3, team4
-import team5, team6, team7, team8, team9
-import team10, team11, team12, team13, team14
 betray = example1
 collude = example0
 
-modules = [example0, example1, example2, example3, example4, example5, example6, example7,
-team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
-team11, team12, team13, team14]
+modules = [example0, example1, example2, example3, example4, example5, example6, example7]
 for module in modules:
-    reload(module)
+    imp.reload(module)
     print ('reloaded',module)
     for required_variable in ['team_name', 'strategy_name', 'strategy_description']:
         if not hasattr(module, required_variable):
@@ -270,7 +266,7 @@ def make_section2(modules, scores):
                               'P'+str(index),
                               str(sum(scores[index])/len(modules)),
                               str(modules[index].strategy_name)))
-    section2_list.sort(key=lambda x: int(x[2]), reverse=True)
+    section2_list.sort(key=lambda x: x[2], reverse=True)
     
     # Generate one string per team
     # Rockettes (P1):  -500 points with Backstabber
@@ -378,5 +374,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
  
 ### Call main_play() if this file is executed
 if __name__ == '__main__':
-    scores, moves, reports = main_play(modules[0:4])   
+    scores, moves, reports = main_play(modules[0:-1])   
     section0, section1, section2, section3 = reports
